b5cce6948aa9e030483b7e4c7e09e3e70232dd37
zhenze <liuzhenze.de@gmail.com>
2024-01-19 13:51:32 +0100

    input via .npy added

--- src/mlifio.c
+++ src/mlifio.c
@@ -43,7 +43,7 @@ MLIF_IO_STATUS mlifio_to_file(const mlif_file_mode fmode, const char *file_path,
         
         char buffer[128] = "";
         char header[128] = "";
-        sprintf(buffer, "{'descr': '<%c%d', 'fortran_order': %s, 'shape': (%lu, %lu), }", type, size, order, row, col);
+        sprintf(buffer, "{'descr': '<%c%d', 'fortran_order': %s, 'shape': (%llu, %llu), }", type, size, order, row, col);
         sprintf(header, "%-*s", 118, buffer);
         header[117] = '\n';
 
@@ -117,7 +117,7 @@ MLIF_IO_STATUS mlifio_to_stdout(const mlif_stdio_mode mode, const mlif_data_conf
         const char tail[] = "\n";
         for (size_t i = 0; i < row; i++)
         {
-            sprintf(header, "Output[%lu]:", i);
+            sprintf(header, "Output[%llu]:", i);
             fwrite(header, sizeof(char), strlen(header), stdout);
             for (size_t j = 0; j < col*size; j++)
             {
@@ -141,11 +141,79 @@ MLIF_IO_STATUS mlifio_to_stdout(const mlif_stdio_mode mode, const mlif_data_conf
     return MLIF_IO_SUCCESS;
 }
 
-MLIF_IO_STATUS mlifio_from_file(const mlif_file_mode fmode, const char *file_path, const mlif_data_config *config, void *data)
+/**
+ * @brief Interface get 2-dimensional input data via file system (.npy or .bin)
+ * 
+ * @param fmode Either MLIF_FILE_BIN or MLIF_FILE_NPY.
+ * @param file_path Path including file name and suffix to store.
+ * @param config Data configuration which contains datatype, shape and further informations.
+ * @param data Data pointer
+ * @return MLIF_IO_STATUS Either MLIF_IO_ERROR or MLIF_IO_SUCCESS.
+ */
+MLIF_IO_STATUS mlifio_from_file(const mlif_file_mode fmode, const char *file_path, mlif_data_config *config, void *data)
 {
+    const char mode[] = "rb";
+    char dtype[3];
+
     if (fmode == MLIF_FILE_NPY)
     {
+        char tmp;
+        char buffer[10] = {};
+        size_t size = 1;
+        size_t row = 0;
+        size_t col = 0;
+        int cnt = 0;
+        short offset = 0;
+        char header_length[2] = {};
+        FILE *file_ptr = NULL;
+        file_ptr = fopen(file_path, mode);
+        if (file_ptr == NULL) return MLIF_IO_ERROR;
+        fseek(file_ptr, 8, SEEK_SET);
+        fread(header_length, sizeof(char), 2, file_ptr);
+        offset = header_length[0] + 256 * header_length[1];
 
+        fseek(file_ptr, 12, SEEK_CUR);
+        fread(dtype, sizeof(char), 2, file_ptr);
+        if (!strcmp(dtype, "i1")) {config->dtype = MLIF_DTYPE_INT8; size = 1;}
+        else if (!strcmp(dtype, "i2")) {config->dtype = MLIF_DTYPE_INT16; size = 2;}
+        else if (!strcmp(dtype, "i4")) {config->dtype = MLIF_DTYPE_INT32; size = 4;}
+        else if (!strcmp(dtype, "u1")) {config->dtype = MLIF_DTYPE_UINT8; size = 1;}
+        else if (!strcmp(dtype, "u2")) {config->dtype = MLIF_DTYPE_UINT16; size = 2;}
+        else if (!strcmp(dtype, "u4")) {config->dtype = MLIF_DTYPE_UINT32; size = 4;}
+        else if (!strcmp(dtype, "f4")) {config->dtype = MLIF_DTYPE_FLOAT; size = 4;}
+        else {config->dtype = MLIF_DTYPE_RAW; size = 1;}
+
+        fseek(file_ptr, 20, SEEK_CUR);
+        if (fgetc(file_ptr) == 'F') config->order = MLIF_C_ORDER;
+        else config->order = MLIF_FORTRAN_ORDER;
+
+        fseek(file_ptr, 16, SEEK_CUR);
+        tmp = fgetc(file_ptr);
+        while (tmp != ',')
+        {
+            buffer[cnt] = tmp;
+            tmp = fgetc(file_ptr);
+            cnt++;
+        }
+        row = atoi(buffer);
+
+        fseek(file_ptr, 1, SEEK_CUR);
+        cnt = 0;
+        memset(buffer, 0, sizeof(buffer));
+        tmp = fgetc(file_ptr);
+        while (tmp != ')')
+        {
+            buffer[cnt] = tmp;
+            tmp = fgetc(file_ptr);
+            cnt++;
+        }
+        col = atoi(buffer);
+        config->row = row;
+        config->col = col;
+
+        fseek(file_ptr, offset+10, SEEK_SET);
+        fread(data, sizeof(char), row*col*size, file_ptr);
+        fclose(file_ptr);
     }
     else if (fmode == MLIF_FILE_BIN)
     {
@@ -192,6 +260,7 @@ MLIF_IO_STATUS mlifio_from_stdin(const mlif_stdio_mode mode, mlif_data_config *c
             cnt++;
         }
         config->row = cnt+1;
+        fflush(stdin);
     }
     else if (mode == MLIF_STDIO_BIN)
     {
@@ -200,6 +269,7 @@ MLIF_IO_STATUS mlifio_from_stdin(const mlif_stdio_mode mode, mlif_data_config *c
         size_t length = 0;
         length = fread(buffer, sizeof(char), col, stdin);
         memcpy(data, buffer, length);
+        fflush(stdin);
     }
     else
     {
